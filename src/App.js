a1c3e3e9f0c04ca006bb4072d4dcd49e5f3b9a66
naveenprakashkv <naveen.prakash.kv@gmail.com>
2024-12-29 12:45:40 +0530

    first message

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from "./logo.svg";
+import "./App.css";
+import PersonalDetails from "./PersonalDetails";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="main-heading">Hello from Pallavi Suresh</h1>
+      <h2 style={{ color: "violet" }}>DevOps Engineer</h2>
+      <h3 style={{ color: "blueviolet" }}> My-First-React-Application</h3>
+      <img
+        className="image"
+        src="http://localhost:3000/logo512.png"
+        alt="img"
+      />
+      <PersonalDetails />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/PersonalDetails.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PersonalDetails = () => {
+  return (
+    <div>
+      <h2>Software</h2>
+    </div>
+  );
+};
+
+export default PersonalDetails;
