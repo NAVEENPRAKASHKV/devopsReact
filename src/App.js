822ae5258259f572058f9adf7ea58b66cc1dadb0
naveenprakashkv <naveen.prakash.kv@gmail.com>
2024-12-29 12:47:28 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -8,11 +8,7 @@ function App() {
       <h1 className="main-heading">Hello from Pallavi Suresh</h1>
       <h2 style={{ color: "violet" }}>DevOps Engineer</h2>
       <h3 style={{ color: "blueviolet" }}> My-First-React-Application</h3>
-      <img
-        className="image"
-        src="http://localhost:3000/logo512.png"
-        alt="img"
-      />
+      <img className="imag" src="http://localhost:3000/logo512.png" alt="img" />
       <PersonalDetails />
     </div>
   );
